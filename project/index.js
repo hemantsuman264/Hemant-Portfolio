120cb60ee7163c27340b8bfa1c7f11e61acb1da4
hemant_suman264 <hs938965@gmail.com>
2025-05-16 00:41:09 +0530

    Updating personal details & fixing responsivenes

--- project/index.js
+++ project/index.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Web Developer"],
+    strings: ["Web Developer", "Tech Learner", "UI/UX Designer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
